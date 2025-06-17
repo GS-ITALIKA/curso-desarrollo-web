8a7365ac6c26f872adf94d0d108d357d9e25ce46
Eric A. Rodríguez S. <eazicomservicios@gmail.com>
2025-06-17 13:16:32 -0600

    Add: Práctica 5.

--- practicas/practica-04/js/index.js
+++ practicas/practica-04/js/index.js
@@ -1,46 +1,90 @@
 //@ts-check
 
-const Message = {
-
-    /** @type {string} */
-    INVALID_EMAIL: 'La dirección de correo electrónico no es válida.',
-
-    /** @type {string} */
-    MIN_LENGTH: 'La contraseña debe tener mínimo 8 caracteres.',
-
-    /** @type {string} */
-    REQUIRED_FIELD: 'Este es un campo obligatorio.',
-
-    TERMS_AND_CONDITIONS: 'Debes aceptar los términos y condiciones.',
-
-    NO_OPTION_SELECTED: 'Selecciona una opción.'
-}
-
+//#region -- Definición de enumeraciones --
 /**
- * Formulario de registro.
+ * Describe un error en la validación del formulario.
+ * @readonly
+ * @enum {string}
+ */
+const Message = {
+    /**
+     * Mensaje de error para una dirección de correo electrónica no válida.
+     * @type {string}
+     */
+    INVALID_EMAIL: 'La dirección de correo electrónico no es válida.',
+    /**
+     * Mensaje de error para una contraseña con menos de 8 caracteres.
+     *  @type {string}
+     */
+    MIN_LENGTH: 'La contraseña debe tener mínimo 8 caracteres.',
+    /**
+     * Mensaje de error para un campo requerido.
+     * @type {string}
+     */
+    REQUIRED_FIELD: 'Este es un campo obligatorio.',
+    /**
+     * Mensaje de error para aceptar los términos y condiciones.
+     * @type {string}
+     */
+    TERMS_AND_CONDITIONS: 'Debes aceptar los términos y condiciones.'
+}
+//#endregion
+
+//#region -- Declaración de constantes --
+/**
+ * Elemento HTML que representa el formulario de registro.
  * @type {HTMLFormElement}
  */
 const REGISTRATION_FORM = /** @type {HTMLFormElement} */ (
     getFormControl( 'registration-form' )
 );
-
+/**
+ * Elemento HTML que representa el botòn para reestablecer el formulario.
+ * @type {HTMLButtonElement}
+ */
+const RESET_BUTTON = /** @type {HTMLButtonElement} */ (
+    getFormControl( 'reset-button' )
+);
 //#endregion
 
+//#region -- Definición de funciones --
 /**
- * 
- * @param {string} id Identificador en el DOM del elemento HTML.
- * @returns {HTMLElement}
+ * Quita los mensajes de error que se hayan producido al validar el formulario.
+ * @returns {void}
+ */
+function clearErrors() {
+    document.querySelectorAll( 'span' ).forEach( span => span.remove() );
+}
+/**
+ * Obtiene el elemento HTML que representa al control de usuario especificado
+ * por su identificador.
+ * @param {string} id Identificador del elemento HTML.
+ * @returns {HTMLElement} El elemento 
  */
 function getFormControl( id ) {
     return /** @type {HTMLElement} */ ( document.getElementById( id ) );
 }
-
 /**
- * 
+ * Reestablece el valor inicial de los controles de usuario.
+ * @returns {void}
+ */
+function resetFormControls() {
+    document.querySelectorAll( 'input' ).forEach( input => {
+        input.checked ? input.checked = false
+                      : input.value = '';
+    } );
+
+    /** @type {HTMLSelectElement} */ (
+        getFormControl( 'options-select' ) ).selectedIndex = 0;
+    
+    
+}
+/**
+ * Crea un mensaje de error para un control cuyo valor no es válido.
  * @param {HTMLElement} control Control de usuario que produjo el error.
  * @param {string} message Mensaje de error.
  */
-function error( control, message ) {
+function setError( control, message ) {
     
     const span = document.createElement( 'span' );
     span.classList.add( 'error' );
@@ -49,15 +93,11 @@ function error( control, message ) {
 
     /** @type {HTMLDivElement} */ ( control.parentElement ).append( span );
 }
-
-const clearErrors = () => {
-    document.querySelectorAll( 'span' ).forEach( span => span.remove() );
-}
-
 /**
- * 
+ * Valida los campos del formulario.
+ * @returns {boolean}
  */
-const validateForm = () => {
+function validateForm() {
     let isValid = true;
     document.querySelectorAll( 'input' ).forEach( input => {
         switch( input.type ) {
@@ -65,35 +105,35 @@ const validateForm = () => {
             case 'date':
                 if ( input.value === '' ) {
                     isValid = false;
-                    error( input, Message.REQUIRED_FIELD );
+                    setError( input, Message.REQUIRED_FIELD );
                 }
                 break;
             case 'password':
                 if ( input.value === '' ) {
                     isValid = false;
-                    error( input, Message.REQUIRED_FIELD );
+                    setError( input, Message.REQUIRED_FIELD );
                 
                 } else if ( input.value.length < 8 ) {
                     isValid = false;
-                    error( input, Message.MIN_LENGTH );
+                    setError( input, Message.MIN_LENGTH );
                 }
                 break;
             case 'email': {
                 const pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
                 if ( input.value === '' ) {
                     isValid = false;
-                    error( input, Message.REQUIRED_FIELD );
+                    setError( input, Message.REQUIRED_FIELD );
                 
                 } else if ( !pattern.test( input.value ) ) {
                     isValid = false;
-                    error( input, Message.INVALID_EMAIL );
+                    setError( input, Message.INVALID_EMAIL );
                 }
                 break;
             }
             case 'checkbox':
                 if ( !input.checked ) {
                     isValid = false;
-                    error( input, Message.TERMS_AND_CONDITIONS );
+                    setError( input, Message.TERMS_AND_CONDITIONS );
                 }
                 break;
         }
@@ -105,17 +145,29 @@ const validateForm = () => {
     );
     if ( select.selectedIndex === 0 ) {
         isValid = false
-        error( select, Message.REQUIRED_FIELD );
+        setError( select, Message.REQUIRED_FIELD );
     }
     return isValid;
 }
+//#endregion
 
+//#region -- Evento principal -- 
+/**
+ * Punto de entrada a la aplicación.
+ */
 document.addEventListener( 'DOMContentLoaded', ev => {
     
     REGISTRATION_FORM.addEventListener( 'submit', ev => {
         ev.preventDefault();
         clearErrors();
-        validateForm();
+        if ( validateForm() ) {
+            resetFormControls();
+            alert( 'La información del formulario es válida.' );
+        }
     } );
 
+    RESET_BUTTON.addEventListener( 'click', ev => {
+        clearErrors();
+    } );
 } );
+//#endregion
