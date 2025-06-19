dd345ed777826163edd3512d0049045f722a05e7
Eric A. Rodríguez S. <eazicomservicios@gmail.com>
2025-06-19 14:07:05 -0600

    Add: Práctica final.

--- practica-final/js/index.js
+++ practica-final/js/index.js
@@ -11,7 +11,14 @@
  * @param {number} limit 
  */
 async function getEmployees( limit ) {
-    
+    const response = await fetch( `https://randomuser.me/api/?results=${limit}` );
+    if ( !response.ok ) {
+        throw new Error( 'Network response was not ok' );
+    }
+    const data = await response.json();
+    return data.results;
 }
 
-document.addEventListener('DOMContentLoaded', () => {})
+document.addEventListener( 'DOMContentLoaded', () => {
+    
+} );
