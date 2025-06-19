31f7d8817b9c3afb11da9b82c17a44ae10ec1968
Eric A. Rodríguez S. <eazicomservicios@gmail.com>
2025-06-19 10:46:27 -0600

    Add: Práctica final.

---
+++ practica-final/js/index.js
@@ -0,0 +1,17 @@
+//@ts-check
+
+/**
+ * Desarrollo Web | IT Academy
+ * @author 891830 - Eric Adalberto Rodríguez Sánchez
+ * @description Práctica final del curso.
+ */
+
+/**
+ * 
+ * @param {number} limit 
+ */
+async function getEmployees( limit ) {
+    
+}
+
+document.addEventListener('DOMContentLoaded', () => {})
---
+++ practica-final/js/registry.js
@@ -0,0 +1,8 @@
+//@ts-check
+
+/**
+ * Desarrollo Web | IT Academy
+ * @author 891830 - Eric Adalberto Rodríguez Sánchez
+ * @description Práctica final del curso.
+ */
+
